435fcb890daf9959cc7eaa86854b0b83a9592177
avanza <avanza@avanza-PC>
2014-05-12 11:29:10 +0100

    cuentatras

---
+++ src/de100a0/De100a0.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de100a0;
+
+/**
+ *
+ * @author avanza
+ */
+public class De100a0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int contador=1;
+        int num=100;
+        while ( contador <= 101){
+                
+                    contador++;
+                    System.out.println(num);
+                    num--;
+        }
+        
+        
+        
+    }
+}
